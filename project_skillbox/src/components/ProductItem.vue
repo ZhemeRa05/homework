5794ed7a74fbf4c120532899e4f4f22b6f6b8082
Олег Жемерин <oleg-zhemerin@mail.ru>
2023-01-03 16:52:23 +0300

    fixed errors

--- project_skillbox/src/components/ProductItem.vue
+++ project_skillbox/src/components/ProductItem.vue
@@ -1,25 +1,13 @@
 <template>
-  <ul class="catalog__list">
-          <li class="catalog__item" v-for="(product, index) in products" :key="index">
-            <a class="catalog__pic" href="#">
-              <img :src="product.image"  :alt="product.title">
-            </a>
-
-            <h3 class="catalog__title">
-              <a href="#">
-                {{ product.title }}
-              </a>
-            </h3>
-
-            <span class="catalog__price">
-              {{ product.price }}
-            </span>
-            </li>
-            </ul>
+  <ProductList :products="products"></ProductList>
 </template>
 
 <script>
+
+import ProductList from './ProductList.vue';
+
 export default {
   props: ['products'],
+  components: { ProductList },
 };
 </script>
--- project_skillbox/src/components/ProductList.vue
+++ project_skillbox/src/components/ProductList.vue
@@ -1,16 +1,30 @@
 <template>
   <ul class="catalog__list">
-          <ProductItem :products="products"></ProductItem>
-  </ul>
+    <li class="catalog__item" v-for="(product, index) in products" :key="index">
+      <a class="catalog__pic" href="#">
+        <img :src="product.image"  :alt="product.title">
+      </a>
+
+      <h3 class="catalog__title">
+        <a href="#">
+          {{ product.title }}
+        </a>
+      </h3>
+
+      <span class="catalog__price">
+        {{ product.price }}
+      </span>
+      </li>
+      </ul>
+      <ProductItem :products="product"></ProductItem>
 </template>
 
 <script>
-
 import ProductItem from './ProductItem.vue';
 
 export default {
   props: ['products'],
-
+  // eslint-disable-next-line vue/no-unused-components
   components: { ProductItem },
 };
 </script>
